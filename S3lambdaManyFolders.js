92c9b053226edefde04826332e207870853c33ea
Stephen Borsay <borsay@gmail.com>
2020-03-08 13:15:05 -0700

    Create S3lambdaManyFolders.js

---
+++ S3lambdaManyFolders.js
@@ -0,0 +1,17 @@
+    var AWS = require('aws-sdk');
+     var s3 = new AWS.S3();
+   
+        exports.handler = (event, context, callbac) => {  
+            var bucketName = '<YOUR-BUCKET-NAME>/<YOUR-FOLDER-NAME>' + "/" + Date.now();
+            var keyName = 'myEvent';
+            var content = JSON.stringify(event);
+        
+            var params = { Bucket: bucketName, Key: keyName, Body: content };
+        
+            s3.putObject(params, function (err, data) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log("Successfully saved object to " + bucketName + "/" + keyName);
+            });
+        };
