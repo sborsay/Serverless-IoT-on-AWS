63afa6601c570e3e7a721780916287687e2eb24a
Stephen Borsay <borsay@gmail.com>
2020-03-08 14:22:45 -0700

    Create My-Arduino-Proxy.js

---
+++ API_Gateway_Direct/My-Arduino-Proxy.js
@@ -0,0 +1,21 @@
+        var AWS = require('aws-sdk');
+        var s3 = new AWS.S3();
+    
+        exports.handler = (event, context, callback) => {  
+            var bucketName = "<Your-Bucket-Name/<Your-folder-Name>";
+            var keyName = JSON.stringify(Date.now());
+            //var content = JSON.stringify(event);
+          var content = JSON.stringify(event.queryStringParameters);
+           
+        
+        
+            var params = { Bucket: bucketName, Key: keyName, Body: content};
+        
+            s3.putObject(params, function (err, data) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log("Successfully saved object to " + bucketName + "/" + keyName 
+                                                                + "and data=" + JSON.stringify(content));
+            });
+        };
