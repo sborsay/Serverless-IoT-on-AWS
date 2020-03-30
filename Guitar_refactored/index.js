cfebdc793b337b0d7946018ff463cd0fe7eb9264
Stephen Borsay <borsay@gmail.com>
2020-03-30 14:54:24 -0700

    Update index.js

--- Guitar_refactored/index.js
+++ Guitar_refactored/index.js
@@ -1,38 +1,39 @@
  //new lambda for data enhancemant uptime to ISO8601 transformm for Devan Sabaratnam's Guitar Project
  //https://hackernoon.com/building-an-iot-dashboard-using-the-onion-omega-and-amazon-aws-a3520f850c9
  //Lambda added for course on serverless IoT on AWS at Udemy.com
-//call this Lmabda fnction index.js in Node for the aws-sdk
+ //call this Lmabda fnction index.js in Node for the aws-sdk
  
- // Load the SDK for JavaScript
+// Load the SDK for JavaScript
 const AWS = require('aws-sdk');
-
-const docClient = new AWS.DynamoDB.DocumentClient({region: '<YOUR-REGION-HERE>'});
-var myDate = new Date();
-var ISO_Date = myDate.toISOString();
+// Set the Region 
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
 
 exports.handler = function(event, context, callback) {
+    
+    //let myDate = new Date();
+      let ISO_Date = new Date().toISOString();  //converted to a single line for the pedantics
     
     
     var params = {
         
       Item: {  
-     //  PARTITION_KEY: {'S': 'id'},
-     // SORT_KEY: {'N': 'timestamp'},
+     //  PARTITION_KEY: {'S': 'id'},    
+     //  SORT_KEY: {'N': 'timestamp'},
       
-     id: "<YOUR-DynamoDB-ID-HERE>", //in our case we just used 'outTopic' from the device publish topic
+     id: "outTopic",
      timestamp:  Date.now(),
    
    payload:{ 
     
-       "datetime":  ISO_Date, 
-       "temperature":   event.temperature, 
-       "humidity":  event.humidity
+       "datetime":    ISO_Date, 
+       "temperature": event.temperature,
+       "humidity":    event.humidity
     
    },
 
  },
            
-           TableName: '<YOUR-DYNAMODB-TABLE-NAME-HERE>'
+           TableName: 'myDDBTable'
         
     };
     
