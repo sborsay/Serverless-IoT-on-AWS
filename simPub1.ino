e1fc7460ca750841f06fc1e45d436f7b9075e51e
Stephen Borsay <borsay@gmail.com>
2020-04-28 18:03:23 -0700

    Update simPub1.ino

--- simPub1.ino
+++ simPub1.ino
@@ -7,6 +7,7 @@
  * SDK:2.2.1(cfd48f3)/Core:2.5.2-56-g403001e3=20502056/lwIP:STABLE-2_1_2_RELEASE/glue:1.1-7-g82abda3/BearSSL:6b9587f
  * 
  * Author: Anthony Elder 
+ * Modified by Stephen Borsay   device2cloud.net
  * License: Apache License v2
  *
  * Modified by Stephen Borsay for AWS IoT Serverless on Udemy
@@ -89,7 +90,7 @@ const String privatePemKey = \
 // until January 16, 2038 so unless it gets revoked you can leave this as is:
 const String caPemCrt = \
 //-----BEGIN CERTIFICATE-----
-"MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF" \
+"XXXXXXXXXimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF" \
 "ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6" \
 "b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL" \
 "MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv" \
