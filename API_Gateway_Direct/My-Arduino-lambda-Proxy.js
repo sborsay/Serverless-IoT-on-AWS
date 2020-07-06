d4625cb6569590fbf4d311f6dfa0e31b228d2ab9
Stephen Borsay <borsay@gmail.com>
2020-07-06 12:51:43 -0700

    Update My-Arduino-lambda-Proxy.js

--- API_Gateway_Direct/My-Arduino-lambda-Proxy.js
+++ API_Gateway_Direct/My-Arduino-lambda-Proxy.js
@@ -4,9 +4,9 @@
         exports.handler = (event, context, callback) => {  
             var bucketName = "<Your-Bucket-Name>/<Your-folder-Name>";
             var keyName = JSON.stringify(Date.now());
-            //var content = JSON.stringify(event);
-          var content = JSON.stringify(event.queryStringParameters);
-           
+          //var content = JSON.stringify(event);                        //uncomment this statement for testing in lambda
+          //var content = JSON.stringify(event.queryStringParameters);  //uncommnet this statement after integration with API Gateway
+                                                                        //keep only one of the above uncommented!
         
         
             var params = { Bucket: bucketName, Key: keyName, Body: content};
