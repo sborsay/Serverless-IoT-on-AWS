5c9109f10efa633d841183ef0fc85b0fa03fa232
Stephen Borsay <borsay@gmail.com>
2021-12-17 07:24:07 -0800

    Use this if other function published twice

---
+++ Lambda_Publisher/IoT-Publisher_NonAsync.js
@@ -0,0 +1,21 @@
+var AWS = require('aws-sdk');
+
+var iotdata = new AWS.IotData({endpoint: '<YOUR-AWS-IOT-ENDPOINT-HERE>' });
+
+exports.handler = (event) => {
+    console.log("The event object is: " + JSON.stringify(event));
+    var params = {
+        topic: "<Your-Topic-Here>", //intopic_thing_7
+        payload: JSON.stringify(event),           //event.Temperature, event.Humidity, event.Time, event.Device_Id
+        qos: 0
+    };
+
+    return iotdata.publish(params, function(err, data) {
+        if (err) {
+            console.log("If error: " + JSON.stringify(err));
+        }
+        else {
+            console.log("Success");
+        }
+    })
+};
