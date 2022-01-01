03cd6b289ed14a043705a5e84870f99737f15fc1
Stephen Borsay <borsay@gmail.com>
2022-01-01 14:37:53 -0800

    Update sendiotmessage_lambda2.py

--- Level4_design/3_ Asynchronous_IoT_Intermediate/sendiotmessage_lambda2.py
+++ Level4_design/3_ Asynchronous_IoT_Intermediate/sendiotmessage_lambda2.py
@@ -2,20 +2,17 @@
 
 import json
 import boto3
-Websocket_HTTPS_URL = "<Insert-Https-Websocket-Endpoint-Here-With-Prefix>"
-
+Websocket_HTTPS_URL = "https://<Insert-Websocket-Endpoint_Here>"
 client = boto3.client("apigatewaymanagementapi", endpoint_url = Websocket_HTTPS_URL)
 ssm_Client = boto3.client('ssm')
-
-response_ssm = ssm_Client.get_parameter(Name='<Insert_Your_Parameter-Store-string_Here>')
-connectionId =  response_ssm['Parameter']['Value']   #dig into the response blob to get our string value
-
-Test_Message = json.dumps({ "message": "Hello from lambda, hardcoded test message"})
 
 
 def lambda_handler(event, context):
     print(event) 
+    response_ssm = ssm_Client.get_parameter(Name='<Insert_ConnectionId-Parameter-Name-Here>')
+    print("my stored connection id: ", response_ssm['Parameter']['Value'] )
+    connectionId =  response_ssm['Parameter']['Value']  #dig into the response blob to get our string cvalue
+    Test_Message = json.dumps({ "message": "Hello from lambda, hardcoded test message"})
     IoT_Message = json.dumps(event)
-    #AWS post_to_connection API requires 'key=value' arguments
+    #AWS API Gateway API's require 'key=value' arguments
     response = client.post_to_connection(ConnectionId = connectionId, Data = IoT_Message)
-    
