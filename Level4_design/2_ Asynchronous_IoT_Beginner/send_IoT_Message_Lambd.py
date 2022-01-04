534a22106b31c372f76df18ba5153b048da937f6
Stephen Borsay <borsay@gmail.com>
2022-01-04 11:56:42 -0800

    Update send_IoT_Message_Lambd.py

--- Level4_design/2_ Asynchronous_IoT_Beginner/send_IoT_Message_Lambd.py
+++ Level4_design/2_ Asynchronous_IoT_Beginner/send_IoT_Message_Lambd.py
@@ -3,12 +3,13 @@ import boto3
 
  Websocket_HTTPS_URL = "<Your-Websockets-Connection-Endpoint-Here>"
  client = boto3.client("apigatewaymanagementapi", endpoint_url = Websocket_HTTPS_URL)
- connectionId = "<Your-ConnectionID-From-CloudWatch-Here>" #must manually enter for this beginner version
+
  
  Test_Message = json.dumps({ "message": "Hello from lambda, hardcoded test message"})
 
 def lambda_handler(event, context):
-    print(event) 
+    print(event)
+    connectionId = "<Your-ConnectionID-From-CloudWatch-Here>" #must manually enter for this beginner version
     IoT_Message = json.dumps(event)
     #AWS API Gateway API's require 'key=value' arguments
     response = client.post_to_connection(ConnectionId = connectionId, Data = IoT_Message)
