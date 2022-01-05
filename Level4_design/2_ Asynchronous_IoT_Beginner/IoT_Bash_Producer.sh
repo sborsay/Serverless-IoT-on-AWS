23eca3cb179577959ef85288111a80c45784596b
Stephen Borsay <borsay@gmail.com>
2022-01-04 16:11:00 -0800

    Update IoT_Bash_Producer.sh

--- Level4_design/2_ Asynchronous_IoT_Beginner/IoT_Bash_Producer.sh
+++ Level4_design/2_ Asynchronous_IoT_Beginner/IoT_Bash_Producer.sh
@@ -34,7 +34,48 @@ for (( i = 1; i <= $iterations; i++)) {
   #echo "vibration: $VIBRATION"
 
  #use below for AWS CLI V1
- #aws iot-data publish --topic "$mqtttopic" --payload "{\"deviceid\":\"$DEVICE\",\"current_ts\":$CURRENT_TS,\"flow\":$FLOW,\"temp\":$TEMP,\"humidity\":$HUMIDITY,\"vibration\":$VIBRATION}" --profile "$profile" --region "$region"
+ #aws iot-data publish --topic "$mqtttopic" --payload #!/bin/bash
+
+mqtttopic='<Insert-Your-IoT-Topic-Here>'
+iterations=10
+wait=5
+region='<Insert-Your-AWS-Test-Region_Here>'
+profile='default'
+
+for (( i = 1; i <= $iterations; i++)) {
+
+  #CURRENT_TS=`date +%s`
+  #DEVICE="P0"$((1 + $RANDOM % 5))
+  #FLOW=$(( 60 + $RANDOM % 40 ))
+  #TEMP=$(( 15 + $RANDOM % 20 ))
+  #HUMIDITY=$(( 50 + $RANDOM % 40 ))
+  #VIBRATION=$(( 100 + $RANDOM % 40 ))
+  temperature=$(( 15 + $RANDOM % 20 ))
+  humidity=$(( 50 + $RANDOM % 40 ))
+
+  # 3% chance of throwing an anomalous temperature reading
+  if [ $(($RANDOM % 100)) -gt 97 ]
+  then
+    echo "Temperature out of range"
+    TEMP=$(($TEMP*6))
+  fi
+
+  echo "Publishing message $i/$ITERATIONS to IoT topic $mqtttopic:"
+  #echo "current_ts: $CURRENT_TS"
+  #echo "deviceid: $DEVICE"
+  #echo "flow: $FLOW"
+  echo "temperature: $temperature"
+  echo "humidity: $humidity"
+  #echo "vibration: $VIBRATION"
+
+ #use below for AWS CLI V1
+ #aws iot-data publish --topic "$mqtttopic" --payload "{\"temperature\":$temperature,\"humidity\":$humidity}" --profile "$profile" --region "$region"
+ 
+ #use below for AWS CLI V2
+ aws iot-data publish --topic "$mqtttopic" --cli-binary-format raw-in-base64-out --payload "{\"temperature\":$temperature,\"humidity\":$humidity}" --profile "$profile" --region "$region"
+
+  sleep $wait
+} --profile "$profile" --region "$region"
  
  #use below for AWS CLI V2
  aws iot-data publish --topic "$mqtttopic" --cli-binary-format raw-in-base64-out --payload "{\"temperature\":$temperature,\"humidity\":$humidity}" --profile "$profile" --region "$region"
