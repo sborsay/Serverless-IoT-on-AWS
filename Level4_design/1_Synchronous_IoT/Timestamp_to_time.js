c4a2a32f71a24449eac4d05d5e3c36ff131f7c12
Stephen Borsay <borsay@gmail.com>
2022-01-15 15:21:08 -0800

    Create Timestamp_to_time.js

---
+++ Level4_design/1_Synchronous_IoT/Timestamp_to_time.js
@@ -0,0 +1,15 @@
+let unix_timestamp = 1549312452
+// Create a new JavaScript Date object based on the timestamp
+// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+var date = new Date(unix_timestamp * 1000);
+// Hours part from the timestamp
+var hours = date.getHours();
+// Minutes part from the timestamp
+var minutes = "0" + date.getMinutes();
+// Seconds part from the timestamp
+var seconds = "0" + date.getSeconds();
+
+// Will display time in 10:30:23 format
+var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+console.log(formattedTime);
