2c25d2ffa98218988742e70d08bee2802a1296ee
Stephen Borsay <borsay@gmail.com>
2022-04-16 15:03:28 -0700

    Create IoT_Publisher.py

---
+++ Lambda_Publisher/IoT_Publisher.py
@@ -0,0 +1,25 @@
+#https://iotespresso.com/publishing-from-lambda-to-an-aws-iot-topic/
+
+
+import json
+import boto3
+
+client = boto3.client('iot-data', region_name='us-east-2')
+
+
+
+def lambda_handler(event, context):
+    print(event)
+    # TODO implement
+    # Change topic, qos and payload
+    response = client.publish(
+        topic='esp32/sub',
+        qos=1,
+        payload=json.dumps({"foo":"bar"}) #from web input via APi Gateway:  payload=json.dumps(event['queryStringParameters'])
+    )
+    print(response)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Published to topic')
+    }
