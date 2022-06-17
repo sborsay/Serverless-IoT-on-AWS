f9e71efaa7d4a1087d882f8836a9fa85183dc97d
Stephen Borsay <borsay@gmail.com>
2022-06-17 11:19:52 -0700

    Create sagemaker.py
    New for AWS Sagemaker 3.6 as 2.7 is depreciated on AWS

---
+++ Basic_IoT_Analytics/sagemaker.py
@@ -0,0 +1,31 @@
+#Using 'conda_chainer_p36'  Also importing Boto3 no longer required, done by default
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# create IoT Analytics client
+client = boto3.client('iotanalytics')
+
+#after running the code portion above run the code below with your own dataset
+
+
+dataset = "creatds617"
+dataset_url = client.get_dataset_content(datasetName = dataset)['entries'][0]['dataURI']
+
+# start working with the data
+
+df = pd.read_csv(dataset_url)
+df.serversidetimestamp = pd.to_datetime(pd.to_numeric(df.serversidetimestamp), unit='ms')
+df.set_index(df.serversidetimestamp, inplace=True)
+#df.sort_values('serversidetimestamp', inplace=True)
+
+fig, ax = plt.subplots()
+df.temperature.plot(legend=True)
+df.humidity.plot(legend=True)
+plt.show()
+
+print('Average Humidity: ', df.humidity.mean())
+print('Number of samples: ', len(df))
+
+print('Average temperature: ', df.temperature.mean())
+print('Number of samples: ', len(df))
